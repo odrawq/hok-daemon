184dbd616f4c625d88c901b7cbfc1f02f13dd05c
qwardo <odrawq.qwardo@gmail.com>
2025-02-26 22:16:05 +0300

    Bump version to 3.0.0

--- include/version.h
+++ include/version.h
@@ -28,8 +28,8 @@
 #ifndef VERSION_H
     #define VERSION_H
 
-    #define MAJOR_VERSION 2
-    #define MINOR_VERSION 5
-    #define PATCH_VERSION 2
+    #define MAJOR_VERSION 3
+    #define MINOR_VERSION 0
+    #define PATCH_VERSION 0
 
 #endif
